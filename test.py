a139f4185a5a447e3668402d541448f029710c25
Raghav Misra <raghav.m2014@gmail.com>
2021-01-17 15:37:54 -0800

    Add rathul's test

--- test.py
+++ test.py
@@ -3,97 +3,90 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
+# Deta, Fauna, Hoppscotch
+def user_by_username(username): 
+    try: 
+        user = client.query(
+            q.get(
+                q.match(
+                    q.index("user_id_by_users"),
+                    username
+                )
+            )
+        )
+        return {
+            "success": True,
+            "user": user
+        }
+    except:
+        return {
+            "success": False,
+        }
+def projects_by_username(username):
+    project_refs = client.query(
+        q.paginate(
+            q.match(
+                q.index("projects_by_user_id"),
+                int(user_by_username(username)['user']['ref'].id())
+            )
+        )
+    )['data']
 
+    projects = []
+
+    for reference in project_refs:
+        project_from_db = client.query(q.get(reference))
+        projects.append({
+            "data": project_from_db["data"],
+            "ref": project_from_db["ref"].id()
+        })
+    
+    return projects
+
+def contributions_by_username(username):
+    pass
 
 client = FaunaClient(secret="fnAD_puLTVACDfjj6lEi151b_Zh3sXx83qaalyea")
 
+username = "getprojecttest"
 
-project_id = int(287897702374572544)
+# projects = projects_by_username(username)
+# for project in projects:
+#     print(project)
 
-
-client.query(
-    q.get(
-        q.ref(
-            q.collection("projects"), 
-            project_id
-        )
-    )
-)
-data = {
-    "data": {
-        "user_id": 287839756335710720,
-        "shared_ids": [287910298753434120],
-        "name": "Some Project 63",
-        "desc": "Final Project 47",
-            "tasks": [
-            {
-                "name": "Speech",
-                "sub_tasks": [
-                {
-                    "name": "Intro",
-                    "completed": True
-                },
-                {
-                    "name": "Bodies",
-                    "completed": True
-                },
-                {
-                    "name": "Conclusion",
-                    "completed": False
-                }
-                ]
-            },
-            {
-                "name": "Powerpoint",
-                "sub_tasks": [
-                {
-                    "name": "Intro",
-                    "completed": False
-                },
-                {
-                    "name": "Bodies",
-                    "completed": False
-                },
-                {
-                    "name": "Conclusion",
-                    "completed": False
-                }
-                ]
-            },
-            {
-                "name": "Present",
-                "sub_tasks": [
-                {
-                    "name": "Speech",
-                    "completed": False
-                }
-                ]
-            }
-            ]
-        }
-}
-print(
-    client.query(
-        q.update(
-            q.select("ref",
-                client.query(
-                    q.ref(
-                        q.collection("projects"), project_id
-                    )
-                )
-            ),
-            data               
-            
-        )
-    )
-)
-# Update(
-#   Select("ref",
-#     Get(
-#       Match(Index("dept_by_deptno"), 10)
+# project_refs = client.query(
+#     q.paginate(
+#         q.match(
+#             q.index("projects_by_contributor_id"),
+#             int(user_by_username(username)['user']['ref'].id())
+#         )
 #     )
-#   ),
-#   {
-#     data: { loc: "AUSTIN" }
-#   }
-# )
+# )['data']
+
+# print(project_refs)
+# print(user_by_username(username)['user'])
+# user = user_by_username(username)['user']['ref'].id()
+
+# print(user)
+# for thing in user:
+#     print(str(thing) + ":   " + str(user[thing]))
+
+print(
+    # client.query(
+    #     q.paginate(
+            q.match(
+                q.index("projects_by_contributor_id"),
+                int(user_by_username(username)['user']['ref'].id())
+            )
+    #     )
+    # )
+)
+
+project_refs = client.query(
+    q.paginate(
+        q.match(
+            q.index("prµojects_by_user_id"),
+            int(user_by_username(username)['user']['ref'].id())
+        )
+    )
+)['data']
